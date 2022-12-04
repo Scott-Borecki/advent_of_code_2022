a381c20b9a432abd5069c2c686b6e290c459421f
Scott-Borecki <scottborecki@gmail.com>
2022-12-04 16:31:07 -0500

    Refactor day_04

--- day_04/day_04.rb
+++ day_04/day_04.rb
@@ -1,26 +1,22 @@
 class CampCleanup
   attr_reader :data
 
+  DEFAULT_RANGE = [*1..99].freeze
+
   def initialize(txt_file = 'input.txt')
     @data = File.readlines(txt_file).map do |line|
-              assignments = line.chomp.split(',').map { _1.split('-') }.map { _1.map(&:to_i) }
-              assignments.map { (_1[0].._1[1]).to_a }
+              line.chomp.split(/-|, |,/).map(&:to_i).each_slice(2).to_a.map { (_1[0].._1[1]).to_a }
             end
   end
 
   def count_fully_overlapping_assignments
     data.count do |assignments|
-      overlapping_assignments(assignments).size == assignments.first.size ||
-        overlapping_assignments(assignments).size == assignments.last.size
+      assignments.any? { |assignment| DEFAULT_RANGE.intersection(*assignments).size == assignment.size }
     end
   end
 
   def count_overlapping_assignments
-    data.count { |assignments| overlapping_assignments(assignments).size > 0 }
-  end
-
-  def overlapping_assignments(assignments)
-    assignments.first & assignments.last
+    data.count { |assignments| DEFAULT_RANGE.intersection(*assignments).size > 0 }
   end
 end
 
